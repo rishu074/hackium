8a127c888e22f3062c1ae8273e2c92b3f8725a90
NotRoyadma <royadma2314@gmail.com>
2022-09-18 12:21:00 +0530

    card checker success

--- cards/validate.js
+++ cards/validate.js
@@ -1,8 +1,254 @@
+import inquirer from 'inquirer';
+import logUpdate from 'log-update';
+import fs from 'fs';
+import fetch from 'node-fetch';
+import Stripe from 'stripe'
+
 export default function validateCards(args, options) {
     if (options && options.save) {
-        console.log("works fine");
+        inquirer.prompt([
+            {
+                name: "filesFromCardsToTake",
+                type: 'input',
+                default: 'generatedCards.txt',
+                message: "Enter the filename with cards in Current Directory"
+            },
+            {
+                name: 'filenameToSaveTheOutput',
+                type: 'input',
+                default: 'checkedCards.txt',
+                message: "Enter the filename to save the output in Current Directory"
+            },
+            {
+                name: 'skKey',
+                type: 'input',
+                default: 'sk_key_fake',
+                message: "Enter the sk key",
+                validate: validateSkKey
+            },
+            {
+                message: "Are you ready?",
+                type: 'confirm',
+                name: "areYouReady",
+                default: 'n'
+            }
+        ]).then(async (answers) => {
+            //loading inside the console
+            console.clear();
+
+            logUpdate(`Progress [ --- ] 15%\n\nReading the Data...`);
+
+            //checking the files
+            logUpdate(`Progress [ ----- ] 25%\n\nChecking the files...`);
+
+            //check for output file
+            if (fs.existsSync(`./${answers.filenameToSaveTheOutput.toString()}`)) {
+                fs.unlinkSync(`./${answers.filenameToSaveTheOutput.toString()}`)
+            }
+
+            //check for input file
+            if (!fs.existsSync(`${answers.filesFromCardsToTake.toString()}`)) {
+                console.log('\nNo input file Found!')
+                return process.exit(1);
+            }
+
+            //get the data from the input file
+            logUpdate(`Progress [ ------- ] 35%\n\nReading Cards...`);
+
+            let readedData;
+
+            try {
+                readedData = fs.readFileSync(`${answers.filesFromCardsToTake.toString()}`, { encoding: 'utf-8' })
+
+                readedData = readedData.split('\n');
+
+                if (readedData.length === 0) {
+                    console.error('No data Found.');
+                    return process.exit(1);
+                }
+            } catch (error) {
+                console.error(error);
+                return process.exit(1);
+            }
+
+            logUpdate(`Progress [ --------- ] 45%\n\nChecking Cards...`);
+
+            const frames = ['-', '\\', '|', '/'];
+            let index = 0;
+            let startTime = Date.now()
+            let chargedCount = 0;
+            let declinedCount = 0;
+
+            // let tokenObject = await createToken({
+            //     card_number: readedData[0].split("|")[0],
+            //     month: readedData[0].split("|")[1],
+            //     year: readedData[0].split("|")[2],
+            //     cvv: readedData[0].split("|")[3],
+            //     sk_key: answers.skKey.toString()
+            // })
+
+
+            for (let i = 0; i < readedData.length; i++) {
+                const frame = frames[index = ++index % frames.length];
+                let data = readedData[i].trim()
+                if (data.length != 0) {
+                    let outputString = data;
+                    data = data.split("|");
+                    let [card_number, month, year, cvv] = data;
+
+                    let tokenObject = await createToken({
+                        card_number: card_number.toString(),
+                        month: month,
+                        year: year,
+                        cvv: cvv,
+                        sk_key: answers.skKey.toString()
+                    });
+
+
+                    if (typeof tokenObject === 'object') {
+                        //here we successfully got the token
+                        let charge = await checkCard(tokenObject, 1, 'usd', answers.skKey.toString());
+                        if (typeof charge === 'object') {
+                            logUpdate(`Progress [ ${frame} ] ${Math.floor((i / (readedData.length - 1)) * 100)}% \n${outputString}\n\nReceipt = ${charge.receipt_url.toString()}\nCaptured = ${charge.captured}`)
+                            chargedCount ++;
+                        } else {
+                            logUpdate(`Progress [ ${frame} ] ${Math.floor((i / (readedData.length - 1)) * 100)}% \n${outputString}\n\n${charge.toString()}`)
+                            declinedCount++;
+                        }
+                    } else {
+                        logUpdate(`Progress [ ${frame} ] ${Math.floor((i / (readedData.length - 1)) * 100)}% \n${outputString}\n\n${tokenObject.toString()}`)
+                        declinedCount++;
+                    }
+                }
+            }
+
+            //done
+            let endTime = Date.now();
+            logUpdate(`Progress [ ! ] 100% \nDone, Checked ${chargedCount + declinedCount} Cards in ${(endTime - startTime) / 100}s, charged ${chargedCount} declined ${declinedCount}`)
+        }).catch((error) => {
+            if (error.isTtyError) {
+                process.exit(1);
+            } else {
+                process.exit(1);
+            }
+        });
     } else {
         console.log("Please use --save flag.")
         process.exit(1);
+    }
+}
+
+
+const validateSkKey = async (key) => {
+    // console.log(key === "sk_key_fakes");
+    let sk_key = key.toString();
+    let stripe = Stripe(sk_key)
+
+    let log;
+    let logTwo;
+
+    try {
+        log = await stripe.charges.create({
+            amount: 2000,
+            currency: 'usd',
+            source: 'tok_amex',
+            description: 'My First Test Charge (created for API docs at https://www.stripe.com/docs/api)',
+        })
+    } catch (error) {
+        log = error
+    }
+
+    try {
+        var tokenObject = await stripe.tokens.create({
+            card: {
+                number: '4100287689104978',
+                exp_month: 9,
+                exp_year: 23,
+                cvc: 456,
+            }
+        })
+    } catch (error) {
+        logTwo = error;
+    }
+
+
+    // console.log(await logTwo)
+    if (await logTwo && await logTwo.code && await logTwo.code.toString().includes("test")) {
+        return "This sk only supports test mode charges."
+    }
+
+    if (await log.toString().includes('Invalid API Key provided')) {
+        return "Please Enter a Valid API KEY"
+    } else if (await log.toString().includes('Invalid token id')) {
+        return true;
+    }
+
+    return "Something error occurred";
+}
+
+const createToken = async ({ card_number, month, year, cvv, sk_key }) => {
+    let stripe = Stripe(sk_key.toString())
+    let rateLimitCount = 0;
+    while (true) {
+        try {
+            var tokenObject = await stripe.tokens.create({
+                card: {
+                    number: card_number.toString(),
+                    exp_month: parseInt(month),
+                    exp_year: parseInt(year),
+                    cvc: cvv.toString(),
+                }
+            })
+            // console.log(tokenObject);
+
+            return tokenObject;
+
+
+        } catch (error) {
+            if (rateLimitCount > 150) {
+                return "rate_limit";
+            }
+            // await console.log('error: ' + error.code);
+            // return false;
+            // console.log(error);
+            if (error && error.code && error.code.toString().includes('rate')) {
+                rateLimitCount++;
+            }
+            if (error && error.code != "rate_limit") {
+                return error.code.toString()
+            }
+        }
+
+    }
+}
+
+const checkCard = async (tokenObject, amount, currency, skKey) => {
+    let stripe = Stripe(skKey.toString());
+    let rateLimitCount = 0;
+    while (true) {
+        try {
+            const charge = await stripe.charges.create({
+                amount: parseInt(amount),
+                currency: currency.toString(),
+                source: tokenObject.id,
+                description: 'Buiscit',
+            });
+
+            return charge;
+        } catch (error) {
+            if (rateLimitCount > 150) {
+                return "rate_limit";
+            }
+            // await console.log('error: ' + error.code);
+            // return false;
+            // console.log(error);
+            if (error && error.code && error.code.toString().includes('rate')) {
+                rateLimitCount++;
+            }
+            if (error && error.code != "rate_limit") {
+                return error.code.toString()
+            }
+        }
+
     }
 }
--- load.js
+++ load.js
@@ -1,10 +1,1 @@
-const getRandomBin = () => {
-    let number = Math.floor(Math.random() * (0 + 999999));
-    if (number.toString().length === 6) {
-        return parseInt(number);
-    } else {
-        return false;
-    }
-};
-
-console.log(getRandomBin())
+console.log(typeof {"loda": "fire"})
